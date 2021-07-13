36993a5e505231f80d7226ae9a0318d5906b2f41
4mal1ne <parieiko.vlad@gmail.com>
2021-07-14 00:52:16 +0300

    First commit

---
+++ easy_tasks/operation_with_numbers.py
@@ -0,0 +1,6 @@
+first_val = int(input())
+second_val = int(input())
+
+print(f'The amount of the  first and second values is: {first_val + second_val}')
+print(f'The difference of the  first and second values is: {first_val - second_val}')
+print(f'The product of the  first and second values is: {first_val * second_val}')
